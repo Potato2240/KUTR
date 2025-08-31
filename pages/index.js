bf7f95d17a26edb988ebf4de73fc1c06c98018d1
Potato2240 <kimyunseong100@gmail.com>
2025-09-01 00:08:39 +0900

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: 24, fontFamily: "system-ui"}}>
+      <h1>배포 연결 OK</h1>
+      <p>이 화면이 보이면 라우팅은 정상입니다.</p>
+    </div>
+  );
+}
